00cbaed382407778f99d5d40aefb2fdfe37b1c43
Pham Vu Phuong <phuongpv@scuti.asia>
2023-02-17 16:05:47 +0700

    init code

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,12 @@
 import { UsersModule } from './system/users/users.module';
 import { AuthModule } from './system/auth/auth.module';
-import { mongooseOptions } from './../shared/options/mongoose.options';
-import { constants } from './../shared/constants/constants';
+import { mongooseOptions } from '@shared/options/mongoose.options';
+import { constants } from '@shared/constants/constants';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
-
+import { ConfigModule } from '@nestjs/config';
 import { plugin } from 'mongoose';
 
 // Inject handler and the incrementor of document version "__v"
@@ -25,9 +25,12 @@ plugin(mongooseDelete, { deletedAt: true });
   imports: [
     /* Mongoose DB connection Init */
     MongooseModule.forRoot(
-      process.env.DB_URI.replace('{{dbName}}', process.env.DATABASE_NAME),
+      process.env.DB_URI,
       mongooseOptions,
     ),
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import into other modules
+    }),
     /* Serve static files at Public [eg: index.html, uploads] */
     ServeStaticModule.forRoot({
       rootPath: join('.', 'public'),
--- src/modules/system/mail/mail.module.ts
+++ src/modules/system/mail/mail.module.ts
@@ -1,35 +1,41 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global() // 👈 global module
 @Module({
   imports: [
-    MailerModule.forRoot({
-      // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
-      // or
-      transport: {
-        host: 'smtp.example.com',
-        secure: false,
-        auth: {
-          user: 'user@example.com',
-          pass: 'topsecret',
+    MailerModule.forRootAsync({
+      // imports: [ConfigModule], // import module if not enabled globally
+      useFactory: async (config: ConfigService) => ({
+        // transport: config.get("MAIL_TRANSPORT"),
+        // or
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
         },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
-        options: {
-          strict: true,
+        defaults: {
+          from: `"No Reply" <${config.get('MAIL_FROM')}>`,
         },
-      },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [MailService],
-  exports: [MailService], // 👈 export for DI
+  exports: [MailService],
 })
 export class MailModule {}
